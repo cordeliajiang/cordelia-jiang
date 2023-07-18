2c779e02727b7459577a71cce633c6fbcc8f53c9
Cordelia <jiangcordelia@gmail.com>
2023-07-18 17:11:53 +1000

    added parts of custom fonts and default css to App.css, imported logo and nav icons to NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import NavagationBar from './components/NavBar';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <NavagationBar></NavagationBar>
+      <NavBar></NavBar>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,55 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
-function NavagationBar() {
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  // scroll condition based on scroll Y
+  useEffect(() => {
+    const onScroll = () => {
+      if(window.scrollY > 50){
+        setScrolled(true);
+      }else{
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return() => window.removeEventListener("scroll", onScroll);
+  }, []
+  )
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={''} alt="Logo"/>
+            <img src={logo} alt="Logo"/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <span className='navbar-toggle-icon'/>
         <Navbar.Toggle/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
+            {/* if a link is clicked, highlight it and update the state, otherwise leave it as is */}
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
           </Nav>
           <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={''} alt="replace with github url:xxxx"/></a>
-                <a href="#"><img src={''} alt=""/></a>
-                <a href="#"><img src={''} alt=""/></a>
+                <a href="#"><img src={navIcon1} alt="replace with github url:xxxx"/></a>
+                <a href="#"><img src={navIcon2} alt=""/></a>
+                <a href="#"><img src={navIcon3} alt=""/></a>
               </div>
               <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
           </span>
@@ -32,5 +58,3 @@ function NavagationBar() {
     </Navbar>
   );
 }
-
-export default NavagationBar;
