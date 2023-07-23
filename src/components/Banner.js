9bf0b2b56f1c7b9b7d2c2c267a85f0c6219f6964
Cordelia <jiangcordelia@gmail.com>
2023-07-23 18:50:13 +1000

    added Skills.js, Project.js, and ProjectCard.js, imported ProjectCard to Project.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,16 +4,16 @@ import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from '../assets/img/header-img.svg';
 
 export const Banner = () => {
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
+    const [loopNum, setLoopNum] = useState(0); // show words to display on screen based on index, index 0 is "Front-end Developer"
+    const [isDeleting, setIsDeleting] = useState(false); // initial state is typing letter, not deleting
     const toRotate = [ "Front-end Developer", "Web Developer", "UX Developer" ];
-    const [text, setText] = useState('');
-    const [delta, setDelta] = useState(300 - Math.random() * 100); // speed of next letter comes after previous letter is typed
+    const [text, setText] = useState(''); // letter that is displaying
+    const [delta, setDelta] = useState(300 - Math.random() * 100); // speed of next letter comes after previous letter is typed, deleting is faster than typing
     const period = 2000; // amount of time transitioning between each word
     
     /* this useEffect will run everytime the text gets updated */
-    /* ticker function takes care of typing or deleting letter */
-    /* delta is the interval */
+    /* ticker function takes care of typing or deleting lettr and of the interval between text being updated */
+    /* delta is the interval when tick function runs */
     useEffect(() => {
         let ticker = setInterval(() => {
             tick();
@@ -22,13 +22,36 @@ export const Banner = () => {
         return () => { clearInterval(ticker) };
     }, [text])
 
+    /* tick function */
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting){
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
     return(
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Portfolio</span>
-                        <h1>{"Cordelia Jiang"}<span className="wrap">Front-end Developer</span></h1>
+                        <h1>{"Cordelia Jiang "}</h1>
+                        <h1><span className="wrap">{text}</span></h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur a mi a interdum. Donec eget libero ac libero fermentum viverra ac ut urna. Sed id purus dignissim, convallis neque eget, gravida augue. Phasellus ac vestibulum tellus. Mauris egestas scelerisque imperdiet. Donec suscipit dolor vel auctor posuere. Aliquam eget augue magna. Ut vel mollis sapien. Suspendisse pulvinar neque vel enim ultricies malesuada.</p>
                         <button onClick={() => console.log('Contact')}>Contact <ArrowRightCircle size={25}/></button>
                     </Col>
---
+++ src/components/Projects.js
@@ -0,0 +1,87 @@
+import { Col, Container, Tab, Row, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+
+export const Projects = () => {
+
+    const projects = [
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+    ];
+
+    return(
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Projects</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur a mi a interdum. Donec eget libero ac libero fermentum viverra ac ut urna. Sed id purus dignissim, convallis neque eget, gravida augue. Phasellus ac vestibulum tellus. Mauris egestas scelerisque imperdiet. Donec suscipit dolor vel auctor posuere. Aliquam eget augue magna. Ut vel mollis sapien. Suspendisse pulvinar neque vel enim ultricies malesuada.</p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                            <Nav variant="pills" defaultActiveKey="/home">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {
+                                            projects.map((project, index) => {
+                                                return(
+                                                    <ProjectCard key={index}{...project}/>
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    <Row>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad officiis praesentium aliquid quo, nesciunt culpa dolor deleniti asperiores laudantium doloremque. Nostrum maxime quia quidem aperiam fugiat animi quos voluptates veritatis?
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    <Row>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis cum, tempore libero consequuntur adipisci sequi, illum perferendis cupiditate, architecto quo laborum ducimus! Unde est esse facilis explicabo quis eum.
+                                    </Row>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img className = "background-image-right" src={colorSharp2}/>
+        </section>
+    )
+}
