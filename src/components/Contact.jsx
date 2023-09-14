3701b13f3db75b1fd843f839d84f73b34fbecf66
Cordelia <jiangcordelia@gmail.com>
2023-09-14 17:28:21 +1000

    integrated contact form email sending api in client side to Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,41 +16,44 @@ export const Contact = () => {
         canvas.height = 200 + canvas.width * heightRatio; // add 200: larger min canvas.width.
     }, []);
 
+    /*************************** FORM ***************************/
     const formInitialDetails = {
-        firstName: '',
-        lastName: '',
+        fullName: '',
         email: '',
         message: ''
-    }
-    
+    };
+
     const [formDetails, setFormDetails] = useState(formInitialDetails);
     const [buttonTxt, setButtonTxt] = useState('Send');
     const [status, setStatus] = useState({});
 
     /* only updates the value user entered for its related category, leaving other form details untouched. */
+    /* field names are the category, value is entered by users */
     const onFormUpdate = (category, value) => {
         setFormDetails({
             ...formDetails,
-            [category] : value
-        })
-    }
+            [category] : value,
+        });
+    };
 
+    /* async() as making request to API */
     const handleSubmit = async (e) => {
         e.preventDefault(); // do not want page reloaded when user submits the form
         setButtonTxt('Sending...');
-        let response = await fetch("http://localhost:5000/contact", {
+        let response = await fetch("/api/contact", {
             method: "POST",
             headers: {
-                "Content-Type": "Application/json; charset=utf-8",
+                "Content-Type": "application/json;charset=utf-8",
             },
-            body: JSON.stringify(formDetails),
+            body: JSON.stringify(formDetails)
         });
 
+        let result = await response.json();
+
         setButtonTxt("Send");
-        let result = response.json();
-        
-        setFormDetails(formInitialDetails);
-        if (result.code === 200){
+        setFormDetails(formDetails);
+
+        if (result.code === 200) {
             setStatus({ success: true, message: "Message Sent Successfully." });
         } else {
             setStatus({ success: false, message: "Something Went Wrong, Please Try Again Later." });
@@ -68,26 +71,46 @@ export const Contact = () => {
                         <h2>Get In Touch</h2>
                         <form onSubmit={handleSubmit}>
                             <Row>
-                                <Col sm={6} className="px-1">
-                                    <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                                </Col>
-                                <Col sm={6} className="px-1">
-                                    <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
-                                </Col>
+                                {/* full name */}
                                 <Col sm={12} className="px-1">
-                                    <input type="email" value={formDetails.email} placeholder="Email Adress" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                                    <input 
+                                        required
+                                        type="text" 
+                                        placeholder="Full Name" 
+                                        value={formDetails.fullName} 
+                                        onChange={(e) => onFormUpdate("fullName", e.target.value)} 
+                                    />
                                 </Col>
+                                {/* email */}
                                 <Col sm={12} className="px-1">
-                                    <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)} />
+                                    <input 
+                                        required
+                                        type="email" 
+                                        placeholder="Email Address" 
+                                        value={formDetails.email}
+                                        onChange={(e) => onFormUpdate("email", e.target.value)} 
+                                    />
+                                </Col>
+                                {/* message */}
+                                <Col sm={12} className="px-1">
+                                    <textarea 
+                                        required
+                                        rows="6" 
+                                        placeholder="Message" 
+                                        value={formDetails.message}
+                                        onChange={(e) => onFormUpdate("message", e.target.value)} 
+                                    />
+                                </Col>
+                                {/* submit button */}
+                                <Col sm={12} className="px-1">
                                     <button type="submit"><span>{buttonTxt}</span></button>
-                                </Col>
-                                <Col>
-                                {
-                                    status.message &&
-                                    <Col>
-                                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                                    </Col>
-                                }
+                                    {status.message && (
+                                        <div className="row">
+                                            <p className={status.success === false ? "danger" : "success"}>
+                                                {status.message}
+                                            </p>
+                                        </div>
+                                    )}
                                 </Col>
                             </Row>
                         </form>
