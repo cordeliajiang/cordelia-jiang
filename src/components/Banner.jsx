fcb51a916a8b5858f117317a36c7ab4bf5620456
Cordelia <jiangcordelia@gmail.com>
2023-12-05 21:08:06 +1000

    made banner section (Banner.jsx) responsive, catered for difference screen size

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -27,7 +27,7 @@ export const Banner = () => {
                         {({ isVisible }) =>
                             <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                                 <h1>{"Cordelia Jiang "}</h1>
-                                <h2><span className="wrap"> Front-end Developer </span></h2>
+                                <h3><span className="wrap"> Front-end Developer </span></h3>
                                 <a href = {resume} target = "_blank"><button>Resume<ArrowRightCircle size={25}/></button></a>
                             </div>}
                         </TrackVisibility>
