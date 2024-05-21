71b975323ff5176aaf3de0a5110ddb229145aec8
Cordelia <jiangcordelia@gmail.com>
2024-05-21 23:35:03 +1000

    cleaned up code for Projects.jsx and project.css to make it more concise, readable, and reusable, columns of project cards will automatically expand / shrink on window resizing, achieved design responsiveness for project section from 9999px down to 50px, background image of project section will stay put on window resizing

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,6 @@
 import { Col, Container, Tab, Row, Nav } from "react-bootstrap";
 import { ProgrammingProjectCard } from "./ProgrammingProjectCard";
 import { DesignProjectCard } from "./DesignProjectCard";
-import colorSharp2 from "../assets/img/color-sharp2.png";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
@@ -15,143 +14,128 @@ import 'animate.css';
 import './project.css';
 import TrackVisibility from "react-on-screen";
 
-export const Projects = () => {
+/* decomposed rendering logic for each tab into a separate ProjectSection component */
+/* passing title and projects props to the ProjectSection component */
+const ProjectSection = ({ title, projects }) => (
+    <Tab.Pane eventKey={title.toLowerCase()}>
+        <div className='proj-content'>
+            {projects.map((project, index) => (
+                <div key={index}>
+                    {title === 'Programming' ? (
+                        <ProgrammingProjectCard {...project} />
+                    ) : (
+                        <DesignProjectCard {...project} />
+                    )}
+                </div>
+            ))}
+        </div>
+    </Tab.Pane>
+);
 
-    const programmingProjects = [
-        {
-            title: "Personal Portfolio, 2023",
-            description: "ReactJS, ThreeJS, JavaScript, Bootstrap, CSS, HTML, Nodemailer, Git",
-            imgUrl: programmingImg1,
-            website: "https://github.com/cordeliajiang/react-portfolio",
-            github: "https://github.com/cordeliajiang/react-portfolio",
-        },
-        {
-            title: "Interior Design Gallery Website",
-            description: "HTML, CSS, JavaScript, Bootstrap",
-            imgUrl: projImg2,
-            website: "https://jiangcordelia.wixsite.com/portfolio",
-            github: "https://jiangcordelia.wixsite.com/portfolio",
-        },
-        {
-            title: "Alcohol Review Web Application",
-            description: "Node.js, Laravel, Blade, PHP, SQLite",
-            imgUrl: projImg3,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg1,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg2,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg3,
-        }
-    ];
+const Projects = () => {
+    const tabs = ['Programming', 'Design'];
+    const projects = {
+        Programming: [
+            {
+                title: "Personal Portfolio, 2023",
+                description: "ReactJS, ThreeJS, JavaScript, Bootstrap, CSS, HTML, Nodemailer, Git",
+                imgUrl: programmingImg1,
+                website: "https://github.com/cordeliajiang/react-portfolio",
+                github: "https://github.com/cordeliajiang/react-portfolio",
+            },
+            {
+                title: "Interior Design Gallery Website",
+                description: "HTML, CSS, JavaScript, Bootstrap",
+                imgUrl: projImg2,
+                website: "https://jiangcordelia.wixsite.com/portfolio",
+                github: "https://jiangcordelia.wixsite.com/portfolio",
+            },
+            {
+                title: "Alcohol Review Web Application",
+                description: "Node.js, Laravel, Blade, PHP, SQLite",
+                imgUrl: projImg3,
+            },
+            {
+                title: "Business Startup",
+                description: "Design & Development",
+                imgUrl: projImg1,
+            },
+            {
+                title: "Business Startup",
+                description: "Design & Development",
+                imgUrl: projImg2,
+            },
+            {
+                title: "Business Startup",
+                description: "Design & Development",
+                imgUrl: projImg3,
+            }
+        ],
+        Design: [
+            {
+                title: "Web Portal Application Prototype, 2022",
+                description: "Adobe XD, Indesign, Illustrator",
+                imgUrl: designImg1,
+                website: "https://jiangcordelia.wixsite.com/portfolio/portal-video-render",
+                brochure: designBrochure1,
+            },
+            {
+                title: "Medical Mobile App Prototype, 2020",
+                description: "Adobe XD, Indesign, Illustrator",
+                imgUrl: designImg2,
+                website: "https://jiangcordelia.wixsite.com/portfolio/home-to-hospital",
+                brochure: designBrochure2,
+            },
+            {
+                title: "Cafe Design, 2016",
+                description: "Photoshop, Illustrator, InDesign, ArchiCad",
+                imgUrl: cafeDesign,
+            },
+            {
+                title: "Business Startup",
+                description: "Design & Development",
+                imgUrl: projImg1,
+            },
+            {
+                title: "Business Startup",
+                description: "Design & Development",
+                imgUrl: projImg2,
+            },
+            {
+                title: "Business Startup",
+                description: "Design & Development",
+                imgUrl: projImg3,
+            }
+        ],
+    };
 
-    const designProjects = [
-        {
-            title: "Web Portal Application Prototype, 2022",
-            description: "Adobe XD, Indesign, Illustrator",
-            imgUrl: designImg1,
-            website: "https://jiangcordelia.wixsite.com/portfolio/portal-video-render",
-            brochure: designBrochure1,
-        },
-        {
-            title: "Medical Mobile App Prototype, 2020",
-            description: "Adobe XD, Indesign, Illustrator",
-            imgUrl: designImg2,
-            website: "https://jiangcordelia.wixsite.com/portfolio/home-to-hospital",
-            brochure: designBrochure2,
-        },
-        {
-            title: "Cafe Design, 2016",
-            description: "Photoshop, Illustrator, InDesign, ArchiCad",
-            imgUrl: cafeDesign,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg1,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg2,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg3,
-        }
-    ];
-
-    return(
-        <section className="project" id="projects">
-            <Container>
-                <Row>
-                    <Col>
-                        <TrackVisibility>
-                        {({ isVisible }) =>
-                            <div className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                                <h2>Projects</h2>
-                            </div>}
-                        </TrackVisibility>
-                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pill-tabs">
-                                <Nav.Item>
-                                    <Nav.Link eventKey="first">Programming</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="second">Design</Nav.Link>
-                                </Nav.Item>
-                                {/* <Nav.Item>
-                                    <Nav.Link eventKey="third">Other</Nav.Link>
-                                </Nav.Item> */}
-                            </Nav>
-                            <Tab.Content>
-                                <Tab.Pane eventKey="first">
-                                    <Row>
-                                        <div className='proj-content'>
-                                            {
-                                                programmingProjects.map((project, index) => {
-                                                    return(
-                                                        <ProgrammingProjectCard key={index}{...project}/>
-                                                    )
-                                                })
-                                            }
-                                        </div>
-                                    </Row>
-                                </Tab.Pane>
-                                <Tab.Pane eventKey="second">
-                                    <Row>
-                                        <div className='proj-content'>
-                                            {
-                                                designProjects.map((project, index) => {
-                                                    return(
-                                                        <DesignProjectCard key={index}{...project}/>
-                                                    )
-                                                })
-                                            }
-                                        </div>
-                                    </Row>
-                                </Tab.Pane>
-                                {/* <Tab.Pane eventKey="third">
-                                    <Row>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis cum, tempore libero consequuntur adipisci sequi, illum perferendis cupiditate, architecto quo laborum ducimus! Unde est esse facilis explicabo quis eum.
-                                    </Row>
-                                </Tab.Pane> */}
-                            </Tab.Content>
-                        </Tab.Container>
-                    </Col>
-                </Row>
-            </Container>
-            <img className = "background-image-right" src={colorSharp2} alt="colorSharp2"/>
-        </section>
+    return (
+        <section className="project" id="projects" >
+            <div className="project-container">
+                <TrackVisibility>
+                    {({ isVisible }) =>
+                        <div className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                            <h2>Projects</h2>
+                        </div>}
+                </TrackVisibility>
+                <Tab.Container id="projects-tabs" defaultActiveKey="programming">
+                    <div className="projects-background"></div>
+                    <Nav variant="pills" className="nav-pills" id="pill-tabs">
+                        {tabs.map((tab, index) => (
+                            <Nav.Item key={index}>
+                                <Nav.Link eventKey={tab.toLowerCase()}>{tab}</Nav.Link>
+                            </Nav.Item>
+                        ))}
+                    </Nav>
+                    <Tab.Content>
+                        {tabs.map((tab, index) => (
+                            <ProjectSection key={index} title={tab} projects={projects[tab]} />
+                        ))}
+                    </Tab.Content>
+                </Tab.Container>
+            </div>
+        </section >
     )
 }
+
+export default Projects;
