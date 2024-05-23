bd4dc32efc4bc041ebb5ece5738982810b0f30f1
Cordelia <jiangcordelia@gmail.com>
2024-05-24 00:37:21 +1000

    Improve readability of Skills.jsx and skills.css code.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,63 +1,76 @@
-import { Container, Col, Row } from "react-bootstrap";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
-import colorSharp from "../assets/img/color-sharp.png";
+import React, { useRef, useState, useEffect } from 'react';
+import './skills.css';
+import { IoLogoHtml5, IoLogoCss3 } from 'react-icons/io';
+import { DiJavascript } from 'react-icons/di';
+import { BsGit } from 'react-icons/bs';
+import { FaReact, FaNodeJs, FaPython, FaLaravel } from 'react-icons/fa';
+import { SiBootstrap, SiPhp, SiMysql, SiSqlite, SiPostman, SiAdobexd, SiAdobephotoshop, SiAdobeillustrator, SiAdobeindesign, SiArchicad } from 'react-icons/si';
+import { TbBrandThreejs } from 'react-icons/tb';
+import SkillItem from './SkillItem';
 
 export const Skills = () => {
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
+    // Array to store skills data
+    const skills = [
+        { icon: <FaReact />, title: "ReactJS" },
+        { icon: <TbBrandThreejs />, title: "ThreeJS" },
+        { icon: <DiJavascript />, title: "JavaScript" },
+        { icon: <SiBootstrap />, title: "Bootstrap" },
+        { icon: <IoLogoCss3 />, title: "CSS" },
+        { icon: <IoLogoHtml5 />, title: "HTML" },
+        { icon: <FaNodeJs />, title: "NodeJS" },
+        { icon: <FaPython />, title: "Python" },
+        { icon: <FaLaravel />, title: "Laravel" },
+        { icon: <SiPhp />, title: "PHP" },
+        { icon: <SiMysql />, title: "MySQL" },
+        { icon: <SiSqlite />, title: "SQLite" },
+        { icon: <BsGit />, title: "Git" },
+        { icon: <SiPostman />, title: "Postman" },
+        { icon: <SiAdobexd />, title: "Adobe XD" },
+        { icon: <SiAdobephotoshop />, title: "Photoshop" },
+        { icon: <SiAdobeillustrator />, title: "Illustrator" },
+        { icon: <SiAdobeindesign />, title: "Indesign" },
+        { icon: <SiArchicad />, title: "ArchiCad" }
+    ];
 
-      return (
-        <section className="skill" id="skills">
-            <Container>
-                <Row>
-                    <Col>
-                        <div className="skill-bx">
-                            <h2>Skills</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur a mi a interdum. Donec eget libero ac libero fermentum viverra ac ut urna. Sed id purus dignissim, convallis neque eget, gravida augue. Phasellus ac vestibulum tellus. Mauris egestas scelerisque imperdiet. Donec suscipit dolor vel auctor posuere. Aliquam eget augue magna. Ut vel mollis sapien. Suspendisse pulvinar neque vel enim ultricies malesuada.</p>
-                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
-                                <div className="item">
-                                    <img src={meter1} alt="Image"/>
-                                    <h5>Web Development</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={meter2} alt="Image"/>
-                                    <h5>Brand Identity</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={meter3} alt="Image"/>
-                                    <h5>Logo Design</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={meter1} alt="Image"/>
-                                    <h5>Web Development</h5>
-                                </div>
-                            </Carousel> 
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-            <img className = "background-image-left" src={colorSharp}/>
+
+    const [containerHeightState, setcontainerHeightState] = useState('auto');
+    const [skillsHeightState, setskillsHeightState] = useState('auto');
+    const skillsContentReference = useRef(null);
+    const skillsContainerReference = useRef(null);
+
+    useEffect(() => {
+        const updateHeights = () => {
+            // Ensure skillsContentReference and skillsContainerReference are defined
+            if (skillsContentReference.current && skillsContainerReference.current) {
+                const contentHeight = skillsContentReference.current.clientHeight;
+                const containerHeightState = skillsContainerReference.current.clientHeight;
+
+                // Adjust the height of skills-container if skills-content overflows
+                const heightDiff = contentHeight - containerHeightState;
+                setcontainerHeightState(contentHeight > containerHeightState ? containerHeightState + heightDiff : 'auto');
+
+                // Match skills div height with updated skills-container height
+                setskillsHeightState(containerHeightState);
+            }
+        };
+
+        window.addEventListener('resize', updateHeights);
+        updateHeights();
+
+        return () => window.removeEventListener('resize', updateHeights);
+    }, []); // Run only once on component mount
+
+    return (
+        <section className="skills" id="skills" style={{ minHeight: skillsHeightState }}>
+            <div className="skills-background"></div>
+            <div className="skills-container" ref={skillsContainerReference} style={{ minHeight: containerHeightState }}>
+                <h2>Programming & Design Skills</h2>
+                <div className='skills-content' ref={skillsContentReference}>
+                    {skills.map((skill, index) => (
+                        <SkillItem key={index} icon={skill.icon} title={skill.title} />
+                    ))}
+                </div>
+            </div>
         </section>
-      )
+    );
 }
