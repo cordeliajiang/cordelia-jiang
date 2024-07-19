aebf2b86d4484235ae4a118f530ce9e1117dad64
Cordelia <jiangcordelia@gmail.com>
2024-07-19 23:44:04 +1000

    Chore: Update project assets and adjust script variable
    - Added new images and PDF for projects section.
    - Updated variable names in convert-to-webp.sh from png to jpg.

--- scripts/convert-to-webp.sh
+++ scripts/convert-to-webp.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Directory where PNG images are located in assets folder
-PNG_ASSETS_DIR="./src/assets/img"
+# Directory where JPG images are located in assets folder
+JPG_ASSETS_DIR="./src/assets/img"
 
-# Loop through PNG files in assets folder and convert each to WebP
-for img in "$PNG_ASSETS_DIR"/*.png; do
+# Loop through JPG files in assets folder and convert each to WebP
+for img in "$JPG_ASSETS_DIR"/*.jpg; do
     filename=$(basename "$img")
     filename_noext="${filename%.*}"
-    magick "$PNG_ASSETS_DIR/$filename" "$PNG_ASSETS_DIR/$filename_noext.webp"
+    magick "$JPG_ASSETS_DIR/$filename" "$JPG_ASSETS_DIR/$filename_noext.webp"
 done
 
-# Directory where PNG images are located in public folder
-PNG_PUBLIC_DIR="./public/img"
+# Directory where JPG images are located in public folder
+JPG_PUBLIC_DIR="./public/img"
 
-# Loop through PNG files in public folder and convert each to WebP
-for img in "$PNG_PUBLIC_DIR"/*.png; do
+# Loop through JPG files in public folder and convert each to WebP
+for img in "$JPG_PUBLIC_DIR"/*.jpg; do
     filename=$(basename "$img")
     filename_noext="${filename%.*}"
-    magick "$PNG_PUBLIC_DIR/$filename" "$PNG_PUBLIC_DIR/$filename_noext.webp"
+    magick "$JPG_PUBLIC_DIR/$filename" "$JPG_PUBLIC_DIR/$filename_noext.webp"
 done
